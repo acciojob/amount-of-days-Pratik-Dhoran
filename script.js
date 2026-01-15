54f0b766a00d7aff28939aea9421fb408ad11dd2
Pratik Dhoran <148314312+Pratik-Dhoran@users.noreply.github.com>
2026-01-15 10:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function daysOfYear(year) {
+
+	 if (year % 400 === 0) {
+        return 366;
+    }
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return 366;
+    }
+    return 365;
+	
+}
